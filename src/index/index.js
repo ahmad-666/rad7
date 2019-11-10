6d07f3325dcc9208fcfded0f7f1d5062b4736f32
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-11-10 13:25:34 -0800

    init commit

---
+++ src/index/index.js
@@ -0,0 +1,8 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../framework.scss' ;
+import '../utilities/styles/form.scss'
+import '../init.scss' ;
+//load scripts
+//load videos 
+
