a9488dc970c18c94e9ac8a9ab721533c8ea57a10
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-11-23 11:46:47 -0800

    fix resp issues

--- src/product/scripts/productInfo.js
+++ src/product/scripts/productInfo.js
@@ -1,5 +1,5 @@
 import form from '../../utilities/scripts/form.js' ;
-import ProductSlider from '../../utilities/scripts/productSlider';
+import ProductSlider from '../../utilities/scripts/productSlider.js';
 let productInfo = document.querySelector('form#productInfo') ;
 new ProductSlider(productInfo.querySelector('#info .productSlider'),document.querySelector('.fix_productSlider')) ;
 productInfo.querySelectorAll('.inputWrapper.numberHandler').forEach(numberHandler => new form.NumberHandler(numberHandler))
