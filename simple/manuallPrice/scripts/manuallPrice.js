04be7830b4cd18663042115ec9503b4444502e63
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-02-16 14:36:29 -0800

    manual price

---
+++ simple/manuallPrice/scripts/manuallPrice.js
@@ -0,0 +1,13 @@
+//import form from '../../utilities/scripts/form.js' ;
+let manualForm = document.querySelector('form#manual') ;
+let manualFormObj = {
+    manualForm,
+    inputs:manualForm.querySelectorAll('.validate'),
+    submit:manualForm.querySelector('button[type="submit"]'),
+    modal:null,
+    send:true ,
+    labels:manualForm.querySelectorAll('.labelHandler')
+}
+new FormValidate(manualFormObj.reportForm,manualFormObj.submit,
+    manualFormObj.inputs,manualFormObj.send,manualFormObj.modal) ;
+    manualFormObj.labels.forEach(label=>new LabelHandler(label)) ;
